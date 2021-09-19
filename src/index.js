e1bb5284c58e7d415159593257691bf4a95acfeb
Alex Kong <alexkong0222@gmail.com>
2021-09-19 14:50:46 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var parameter=true;
+
+const testFunction=()=> {
+  return( 
+  <div>
+    <p>Hello World</p>
+    <button onClick={()=>{console.log("Button Click")}}>Button</button>
+    <p id>parameter: {(parameter===true)?"true":"false"}</p>
+  </div>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  testFunction(),
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
