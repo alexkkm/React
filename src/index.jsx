e2b3abae08c3e14df5b912dd0e6b52046a601583
alexkkm <alexkong0222@gmail.com>
2021-10-10 17:38:02 +0800

    init

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CompoentExample from './test';
+
+
+
+ReactDOM.render(
+  <CompoentExample />,
+  document.getElementById('root')
+);
+
+
---
+++ src/test.jsx
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+
+var parameter=true;
+
+class CompoentExample extends Component{
+    render(){
+        return(
+            <div>
+            <p>Hello World</p>
+            <button onClick={()=>{console.log("Button Click")}}>Button</button>
+            <p id>parameter: {(parameter===true)?"true":"false"}</p>
+          </div>
+        )
+    }
+    /* Comment line in JSX */
+}
+export default CompoentExample;
+
+ReactDOM.render(
+    CompoentExample,
+    document.getElementById('root')
+   );
